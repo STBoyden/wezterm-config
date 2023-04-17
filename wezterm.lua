0c374949be61303e9ab6f99887240e8b24014424
Samuel Boyden <sam@stboyden.com>
2023-04-17 20:51:02 +0100

    Move configurations into table

--- wezterm.lua
+++ wezterm.lua
@@ -7,29 +7,28 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
-config.font_size = 13.0
-
-config.disable_default_key_bindings = true
-config.keys = mappings
-
-config.default_prog = { "/var/home/sboyden/.cargo/bin/nu" }
-config.color_scheme = "carbonfox"
-
-config.initial_cols = 120
-config.initial_rows = 30
-
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
-
-config.window_frame = {
-	active_titlebar_bg = "#161616",
-	button_bg = "#161616",
+config = {
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	font_size = 13.0,
+	disable_default_key_bindings = true,
+	keys = mappings,
+	default_prog = { "/var/home/sboyden/.cargo/bin/nu" },
+	warn_about_missing_glyphs = false,
+	-- appearance configuration
+	color_scheme = "carbonfox",
+	initial_cols = 120,
+	initial_rows = 30,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	window_frame = {
+		active_titlebar_bg = "#161616",
+		button_bg = "#161616",
+	},
 }
 
 return config
